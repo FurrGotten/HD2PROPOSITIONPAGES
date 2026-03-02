c33a6c7550b40d3297f9b2f4d20d9364e8500dcb
FurrGotten <FurrGotten@gmail.com>
2026-03-02 18:43:46 +0200

    warbound style base

--- src/routeComponents/WarBoundCreatorPage/WarBoundCreatorPage.tsx
+++ src/routeComponents/WarBoundCreatorPage/WarBoundCreatorPage.tsx
@@ -1,9 +1,53 @@
+import React from 'react';
 import './WarBoundCreatorPageStyle.css';
 
+interface itemType {
+  header: string;
+  subitems: {
+    name: string;
+    image: any;
+  }[];
+}
+
+type itemsType = itemType[];
+
 export function WarBoundCreatorPage() {
-  return <section className="page-base">
+  const [items, setItems] = React.useState<itemsType>([]);
+  // const [armourImage, armourImage] = React.useState<string>('');
 
+  console.log(items, setItems);
 
+  return <section className="wbc-page-base">
+    <section className="warbound-frame">
+      <div className="warbond-header">
+        <div className="line line-left"></div>
+        <span className="text">PREMIUM WARBOND</span>
+        <div className="line line-right"></div>
+      </div>
+      <div className="wb-content">
+        <div className="wb-armor">
+          <div className="frame-banner">
+            <span className="frame-banner-text">ARMOR SETS</span>
+          </div>
+          <div className="wb-armor-frame">
+            <div className="wb-armor-inner">
+
+            </div>
+          </div>
+        </div>
+        <div className="wb-items">
+          <div className="wb-item">
+            <div className="frame-banner">
+              <span className="frame-banner-text">ITEM</span>
+            </div>
+            <div className="wb-item-frame">
+              <div className="wb-item-inner">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </section>;
-
 }
